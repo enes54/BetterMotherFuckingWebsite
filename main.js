d6cce34757f9414108435e14163387c461da3549
Enes54 <enes54@hotmail.fr>
2018-09-26 18:02:35 +0200

    javascript

---
+++ main.js
@@ -0,0 +1,9 @@
+var myVar; //Ma variable
+
+setTimeout(confirmation, 10000); // temps
+function confirmation(){
+  myVar = confirm ("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please.");
+  if (myVar) {
+  window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+}
